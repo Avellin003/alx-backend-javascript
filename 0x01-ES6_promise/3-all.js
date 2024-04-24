404e4858f715c027c398f7bf0efb00c282e9114d
Avellin003 <uavellinbonaparte@gmail.com>
2024-04-24 12:03:49 +0200

    updates task-3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,12 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([
-    uploadPhoto(),
-    createUser(),
-  ]).then((values) => {
-    console.log(values[0].body, values[1].firstName, values[1].lastName);
-  }).catch(() => console.log('Signup system offline'));
+  const a1 = uploadPhoto();
+  const a2 = createUser();
+
+  return Promise.all([a1, a2])
+    .then(([photoResponse, userResponse]) => {
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
