4af86bc325240acbbf203539511dcf9f347e1895
Avellin003 <uavellinbonaparte@gmail.com>
2024-04-30 15:58:27 +0200

    updates

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,14 +1,13 @@
-export default function updateUniqueItems(maps) {
-  /* eslint-disable no-param-reassign */
-  if (mapss instanceof Map) {
-    for (const [key, value] of map) {
+const updateUniqueItems = (maps) => {
+  if (maps instanceof Map) {
+    for (const [key, value] of maps.entries()) {
       if (value === 1) {
         maps.set(key, 100);
       }
     }
-
     return maps;
   }
-
   throw new Error('Cannot process');
-}
+};
+
+export default updateUniqueItems;
