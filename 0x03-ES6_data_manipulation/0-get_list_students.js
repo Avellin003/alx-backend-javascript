840d8b5ea228fddf34af53bdf6641dc566815c5a
Avellin003 <uavellinbonaparte@gmail.com>
2024-04-30 07:53:27 +0200

    Updates

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,6 @@
-export default function getListStudents() {
-  const students = [
-    { id: 1, firstName: 'John', lastName: 'Doe' },
-    { id: 2, firstName: 'Jane', lastName: 'Doe' },
-    { id: 3, firstName: 'Shane', lastName: 'Smith' },
-  ];
-  return students;
-}
+const students = () => [
+  { id: 1, firstName: 'John', lastName: 'Doe' },
+  { id: 2, firstName: 'Jane', lastName: 'Doe' },
+  { id: 3, firstName: 'Shane', lastName: 'Smith' },
+];
+export default students;
