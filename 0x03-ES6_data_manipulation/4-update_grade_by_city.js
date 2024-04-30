19bf5b1fc943fb95041e590554351013b0a88160
Avellin003 <uavellinbonaparte@gmail.com>
2024-04-30 15:21:04 +0200

    updates

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,22 @@
+export default function updateStudentGradeByCity(lists, city, newGrade) {
+  /* eslint-disable no-param-reassign */
+  /* eslint-disable no-prototype-builtins */
+  return lists
+    .filter((object) => object.location === city)
+    .map((student) => {
+
+      newGrade.map((studentGrade) => {
+        if (studentGrade.studentId === student.id) {
+
+          student.grade = studentGrade.grade;
+        }
+
+        if (!student.hasOwnProperty('grade')) {
+          student.grade = 'N/A';
+        }
+        return student;
+      });
+
+      return student;
+    });
+}
