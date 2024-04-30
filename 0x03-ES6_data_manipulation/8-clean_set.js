6e1bcdcbea6ea44ba5e902a9f8e885c701ff6795
Avellin003 <uavellinbonaparte@gmail.com>
2024-04-30 15:55:44 +0200

    upate

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,11 @@
-export default function cleanSet(set, startString) {
-  /* eslint-disable array-callback-return */
+const cleanSet = (set, startString) => {
   if (startString === undefined || startString.length === 0) {
     return '';
   }
   return [...set]
-    .filter((str) => (str !== undefined ? str.startsWith(string) : ''))
-    .map((str) => (str !== undefined ? str.slice(string.length) : ''))
+    .filter((parameter) => (parameter !== undefined ? parameter.startsWith(startString) : ''))
+    .map((parameter) => (parameter !== undefined ? parameter.slice(startString.length) : ''))
     .join('-');
-}
+};
+
+export default cleanSet;
