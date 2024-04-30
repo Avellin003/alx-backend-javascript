875f5bc386a5cf3235bb4f3ff1795fabc95bb45e
Avellin003 <uavellinbonaparte@gmail.com>
2024-04-30 08:06:37 +0200

    Updates

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,6 @@
-const students = () => [
+const getListStudents = () => [
   { id: 1, firstName: 'John', lastName: 'Doe' },
   { id: 2, firstName: 'Jane', lastName: 'Doe' },
   { id: 3, firstName: 'Shane', lastName: 'Smith' },
 ];
-export default students;
+export default getListStudents;
