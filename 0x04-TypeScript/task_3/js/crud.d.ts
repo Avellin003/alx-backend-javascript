1ede7bc48bca5f882443d25465cca2b67c9acce3
Avellin003 <uavellinbonaparte@gmail.com>
2024-05-01 17:17:43 +0200

    task_7

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,5 @@
+import { RowID, RowElement } from './interface';
+
+declare function insertRow(row: RowElement): RowID;
+declare function updateRow(rowId: RowID, row: RowElement): void;
+declare function deleteRow(rowId: RowID): void;
