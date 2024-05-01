6d17e117d57a1385868a651a9d3ecd69a9fab4b9
Avellin003 <uavellinbonaparte@gmail.com>
2024-05-01 16:20:26 +0200

    updates

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -14,3 +14,35 @@ interface Director extends Teacher{
 function printTeacher(firstName: string, lastName: string):string {
     return `${firstName.charAt(0)}. ${lastName}`;
 }
+
+// Interface for the constructor
+interface StudentConstructor {
+    new (firstName: string, lastName: string): StudentInterface;
+}
+
+// Interface for the class
+interface StudentInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+// Class
+class StudentClass implements StudentInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
