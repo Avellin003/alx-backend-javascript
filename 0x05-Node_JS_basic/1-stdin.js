3605fa5e357039db563b3268deb91b67d8693453
Avellin003 <uavellinbonaparte@gmail.com>
2024-06-18 13:51:49 +0000

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,27 +1,16 @@
-// Import the readline module to handle user input
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Create an interface to read from stdin and write to stdout
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-// Display the initial welcome message
-console.log('Welcome to Holberton School, what is your name?');
-
-// Read the user's input
-rl.on('line', (input) => {
-    // Display the user's name
-    console.log(`Your name is: ${input}`);
-    
-    // Close the readline interface
-    rl.close();
-});
-
-// Handle the closing of the readline interface
-rl.on('close', () => {
-    // Display the closing message
-    console.log('This important software is now closing');
-    process.exit(0);
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
