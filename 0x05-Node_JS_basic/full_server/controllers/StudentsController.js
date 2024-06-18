17d45765efb586bf20f1602f86d8b850a094f3ca
Avellin003 <uavellinbonaparte@gmail.com>
2024-06-18 19:35:41 +0000

    udpates

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,5 +1,5 @@
-import { readDatabase } from '../utils';
 import path from 'path';
+0;276;0cimport { readDatabase } from '../utils';
 
 class StudentsController {
   static async getAllStudents(req, res) {
