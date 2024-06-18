ef2a4b8208493bcd90f4d9122432a4077c1be1e9
Bonaparte003 <b.uteramaho@alustudent.com>
2024-06-18 14:13:47 +0200

    updates

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,25 +1,12 @@
-// Import the readline module to handle input and output
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Create an interface for input and output
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
-// Display the welcome message
-console.log('Welcome to Holberton School, what is your name?');
-
-// Prompt the user for their name
-rl.question('', (name) => {
-  // Display the user's name
-  console.log(`Your name is: ${name}`);
-  
-  // Close the readline interface
-  rl.close();
-});
-
-// Listen for the 'close' event to display the closing message
-rl.on('close', () => {
-  console.log('This important software is now closing\n');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
