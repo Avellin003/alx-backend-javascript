cc1d1574658909f58ef7be3e777d756969bc5fb1
Avellin003 <uavellinbonaparte@gmail.com>
2024-06-20 17:34:15 +0000

    udpates

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,21 +1,14 @@
 const express = require('express');
+
 const app = express();
 const port = 7865;
 
-// Middleware to log server start message
-const server = app.listen(port, () => {
-  console.log(`API available on localhost port ${port}`);
+app.get('/', (request, response) => {
+  response.send('Welcome to the payment system');
 });
 
-// Route handling
-app.get('/', (req, res) => {
-  res.send('Welcome to the payment system');
+app.listen(port, () => {
+    console.log("API available on localhost port 7865");
 });
 
-// New endpoint for cart with ID validation
-app.get('/cart/:id(\\d+)', (req, res) => {
-  const cartId = req.params.id;
-  res.send(`Payment methods for cart ${cartId}`);
-});
-
-module.exports = server; // Exporting the server instanc
+module.exports = app;
--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,65 +1,28 @@
-const request = require('request');
-const { expect } = require('chai');
+const request = require("request");
+const {describe, it} = require("mocha");
+const expect = require("chai").expect;
 
-describe('Index page', function () {
-  let server;
-
-  before(function (done) {
-    // Start the server before tests run
-    server = require('./api');
-    server.on('listening', () => {
-      done();
+describe("Index page", function() {
+    const options = {
+	url: "http://localhost:7865/",
+	method: "GET"
+    }
+    it("check correct status code", function(done) {
+	request(options, function(err, res, body) {
+	    expect(res.statusCode).to.equal(200);
+	    done();
+	});
     });
-  });
-
-  after(function (done) {
-    // Close the server after tests complete
-    server.close(function () {
-      done();
+    it("check correct content", function(done) {
+	request(options, function(err, res, body) {
+	    expect(body).to.contain("Welcome to the payment system");
+	    done();
+	});
     });
-  });
-
-  it('Correct status code for /', function (done) {
-    request.get('http://localhost:7865/', function (error, response, body) {
-      expect(response.statusCode).to.equal(200);
-      done();
+    it("check correct content length", function(done) {
+	request(options, function(err, res, body) {
+	    expect(res.headers['content-length']).to.equal('29');
+	    done();
+	});
     });
-  });
-
-  it('Correct result for /', function (done) {
-    request.get('http://localhost:7865/', function (error, response, body) {
-      expect(body).to.equal('Welcome to the payment system');
-      done();
-    });
-  });
-
-  describe('Cart page', function () {
-    it('Correct status code when :id is a number', function (done) {
-      request.get('http://localhost:7865/cart/12', function (error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        done();
-      });
-    });
-
-    it('Correct result when :id is a number', function (done) {
-      request.get('http://localhost:7865/cart/12', function (error, response, body) {
-        expect(body).to.equal('Payment methods for cart 12');
-        done();
-      });
-    });
-
-    it('Correct status code when :id is NOT a number (404)', function (done) {
-      request.get('http://localhost:7865/cart/hello', function (error, response, body) {
-        expect(response.statusCode).to.equal(404);
-        done();
-      });
-    });
-
-    it('Correct result when :id is NOT a number (404 message)', function (done) {
-      request.get('http://localhost:7865/cart/hello', function (error, response, body) {
-        expect(body).to.include('Cannot GET /cart/hello');
-        done();
-      });
-    });
-  });
 });
